3d1ce7a83ac4f003103e1e85368b46b0aab5ff19
Javier <javiergrana@hotmail.com>
2022-09-01 22:07:45 -0300

    Agregado ejemplos de la clase 1 y 2

---
+++ Clase2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLog;
+
+Console.WriteLine("Hello, World!");
+
+
+Logger logger = LogManager.GetCurrentClassLogger();
+
+logger.Trace("Ejemplo de mensaje de trace ");    
+logger.Debug("Ejemplo de mensaje debug ");        
+logger.Info("Ejemplo de mensaje de información");        
+logger.Warn("Ejemplo de mensaje de Warm");        
+logger.Error("Ejemplo de mensaje de error");        
+logger.Fatal("Ejemplo de mensaje de error Fatal");
